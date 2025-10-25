cdad4f14736f1ef1e8ab9113b8b14f86747743a9
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-10-25 09:00:55 +0530

    Refactor user authentication and add subscription model
    - Updated loginUser function to check for either username or email.
    - Added refreshAccessToken and changeCurrentPassword functions.
    - Introduced subscription model for user subscriptions.
    - Modified user routes to include refresh token endpoint.
    - Adjusted package.json to correct server file path.
    - Cleaned up console log formatting in server.js.
    - Removed unused image file from public directory.

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controller/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controller/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
