1b1bed4efa69f5b3ab27c598156b0862b646a3e3
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-10-27 19:39:46 +0530

    Adding more controllers and router along with MongoDB aggregation pipelines.

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -27,4 +27,25 @@ router.route("/logout").post(verifyJWT, logoutUser)
 //refrshToken routes
 router.route("/refresh-token").post(refreshAccessToken)
 
+//changePassword
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+//fetching current user
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+//update account details
+router.route("/update-account").patch(verifyJWT, upadteAccountDetails)
+
+//update avatar image
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+//update cover image
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+//get channel profile
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+//get user watch history
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 export default router;
